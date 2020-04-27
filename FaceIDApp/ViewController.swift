5cda44659c404c18447eaaaaed49b88ba359ac1b
test <test>
2020-04-27 23:12:49 +0900

    first commit

--- FaceIDApp/ViewController.swift
+++ FaceIDApp/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var resultLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  @IBAction func Pressed(_ sender: Any) {
+    
+    let context = LAContext()
+    let reason = "This app uses Touch ID / Facd ID to secure your data."
+    var authError: NSError?
+    
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { (success, error) in
+        if success {
+          self.setMessage("Authenticated")
+        } else {
+          let message = error?.localizedDescription ?? "Failed to authenticate"
+          self.setMessage(message)
+        }
+      }
+    } else {
+      let message = authError?.localizedDescription ?? "canEvaluatePolicy returned false"
+      setMessage(message)
     }
-
-
+  }
+  
+  func setMessage(_ message: String) {
+    DispatchQueue.main.async { [unowned self] in
+      self.resultLabel.text = message
+    }
+  }
+  
 }
-
